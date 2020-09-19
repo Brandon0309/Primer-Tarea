9bbf4cb89a3c4f31a5a001656fcd75bbac003475
Brandon Reyes Alvarado <67602859+Brandon0309@users.noreply.github.com>
2020-09-19 16:07:11 -0600

    First client window

---
+++ src/Client.java
@@ -0,0 +1,53 @@
+import javax.swing.*;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+        ClientWindow window=new ClientWindow();
+
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+    }
+
+}
+
+class ClientWindow extends JFrame{
+
+    public ClientWindow(){
+
+        setBounds(600,300,280,350);
+
+        ClientWindowLayer windowLayer=new ClientWindowLayer();
+
+        add(windowLayer);
+
+        setVisible(true);
+    }
+}
+
+class ClientWindowLayer extends JPanel{
+
+    public ClientWindowLayer(){
+
+        JLabel text=new JLabel("Client");
+
+        add(text);
+
+        textField=new JTextField(20);
+
+        add(textField);
+
+        button=new JButton("Send");
+
+        add(button);
+
+    }
+
+    private JTextField textField;
+
+    private JButton button;
+}
+
